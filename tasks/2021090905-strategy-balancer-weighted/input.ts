7854ddc909e37fab167fc1423ea8f3526288f45c
defioctopus <defioctopus@gmail.com>
2021-09-11 23:18:20 -0300

    increase slippage in some  kovan strategies

--- tasks/2021090905-strategy-balancer-weighted/input.ts
+++ tasks/2021090905-strategy-balancer-weighted/input.ts
@@ -20,7 +20,7 @@ export default {
         poolId: '0xAE4D08895B1F0F14C7AB5F5B2E9BF54E6E1E5C6F000200000000000000000175',
         tokenIndex: 0,
         balToken: '0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7',
-        slippage: fp(0.1),
+        slippage: fp(0.99),
         metadata: 'USDC-50-WETH-50',
       },
       {
@@ -30,7 +30,7 @@ export default {
         poolId: '0x85370D9E3BB111391CC89F6DE344E80176046183000200000000000000000176',
         tokenIndex: 0,
         balToken: '0x41286Bb1D3E870f3F750eB7E1C25d7E48c8A1Ac7',
-        slippage: fp(0.1),
+        slippage: fp(0.99),
         metadata: 'DAI-50-WETH-50',
       },
     ],
