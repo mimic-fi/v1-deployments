2aee64993f5ce48e1d4e3f786ab7525b33438001
lkndjrt <lkndjrt@gmail.com>
2022-03-11 17:01:47 -0300

    agreements: use wMATIC instead of wETH for polygon

--- tasks/2022022302-agreement-factory/input.ts
+++ tasks/2022022302-agreement-factory/input.ts
@@ -21,7 +21,7 @@ export default {
   polygon: {
     from,
     Vault,
-    WETH: '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+    WETH: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', // wMATIC
   },
   kovan: {
     from,
