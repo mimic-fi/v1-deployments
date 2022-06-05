11ee308147b0f830ef4f5e78f8045dd4fd64174b
lkndjrt <lkndjrt@gmail.com>
2022-06-05 07:56:42 -0700

    strategy aave: build task and deployed to mainnet

---
+++ tasks/2022060203-strategy-aave/test/task.deploy.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai'
+import hre from 'hardhat'
+
+import Task from '../../../src/task'
+import { AaveStrategyDeployment } from '../input'
+
+describe('AaveStrategy V2', function () {
+  const task = Task.fromHRE('2022060203-strategy-aave', hre)
+
+  it('deployed an AaveStrategy as expected', async () => {
+    const input = task.input() as AaveStrategyDeployment
+    const output = task.output()
+
+    for (const strategyData of input.strategies) {
+      const strategy = await task.instanceAt('AaveStrategy', output[strategyData.name])
+      expect(await strategy.getVault()).to.be.equal(input.Vault)
+      expect(await strategy.getToken()).to.be.equal(strategyData.token)
+      expect(await strategy.getAToken()).to.be.equal(strategyData.aToken)
+      expect(await strategy.getSlippage()).to.be.equal(strategyData.slippage)
+      expect(await strategy.getMetadataURI()).to.be.equal(strategyData.metadata)
+    }
+  })
+})
