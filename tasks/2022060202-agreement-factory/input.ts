3e208189e975cd1b9dc54ba3f6e727cfa08bf2df
lkndjrt <lkndjrt@gmail.com>
2022-06-05 07:56:04 -0700

    agreement v2: build task and deploy to mainnet

---
+++ tasks/2022060202-agreement-factory/input.ts
@@ -0,0 +1,21 @@
+import Task from '../../src/task'
+
+const Vault = new Task('2022022301-vault')
+
+export type AgreementFactoryDeployment = {
+  from: string
+  Vault: string
+  WETH: string
+}
+
+/* eslint-disable no-secrets/no-secrets */
+
+const from = '0xC1698109c75ee0b11470DCe249d3f490dd5f2A09'
+
+export default {
+  mainnet: {
+    from,
+    Vault,
+    WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  },
+}
