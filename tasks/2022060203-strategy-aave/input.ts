504e7b5240fd9cf3884bf1a9da7d40b525371a70
lkndjrt <lkndjrt@gmail.com>
2022-06-09 18:14:45 -0300

    aave: deploy strategies to polygon

--- tasks/2022060203-strategy-aave/input.ts
+++ tasks/2022060203-strategy-aave/input.ts
@@ -26,7 +26,7 @@ export default {
     lendingPool: '0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf',
     strategies: [
       {
-        name: 'aDAI',
+        name: 'DAI',
         token: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', // DAI
         aToken: '0x27F8D03b3a2196956ED754baDc28D73be8830A6e',
         slippage: fp(0.01),
@@ -41,7 +41,7 @@ export default {
     lendingPool: '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9',
     strategies: [
       {
-        name: 'aDAI',
+        name: 'DAI',
         token: '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
         aToken: '0x028171bCA77440897B824Ca71D1c56caC55b68A3',
         slippage: fp(0.01),
