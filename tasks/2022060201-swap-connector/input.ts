e4c03b9b57cc17d3266026ea1b99a132bc7dbf6a
lkndjrt <lkndjrt@gmail.com>
2022-06-10 10:02:18 -0300

    swap-connector: deploy to polygon

--- tasks/2022060201-swap-connector/input.ts
+++ tasks/2022060201-swap-connector/input.ts
@@ -88,6 +88,28 @@ export default {
     uniswapV2: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',
     uniswapV3: '0xE592427A0AEce92De3Edee1F18E0157C05861564',
     balancerV2: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
-    config: {},
+    config: {
+      balancerV2: [
+        {
+          tokens: [
+            '0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3', // BAL
+            '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // USDC
+          ],
+          poolIds: [
+            '0x0297e37f1873d2dab4487aa67cd56b58e2f27875000100000000000000000002', // WMATIC/USDC/WETH/BAL
+          ],
+        },
+        {
+          tokens: [
+            '0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3', // BAL
+            '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', // WMATIC
+          ],
+          poolIds: [
+            '0xba07d63875d0c2319d43cbb8a897f89bdafd7e87000100000000000000000077', // WMATIC/LINK/DAI/BAL/1INCH/ENS/AAVE/GTC
+          ],
+        },
+      ],
+      uniswapV3: [],
+    },
   },
 }
